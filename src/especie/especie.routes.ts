4b9a60d595d4a65b78a926163733edf298bc359f
Marianela Altamirano <marianelaaltamirano.es@gmail.com>
2026-07-10 16:51:56 -0300

    Agrego endpoint get one de Especie y peticiones Rest Client

--- src/especie/especie.routes.ts
+++ src/especie/especie.routes.ts
@@ -11,3 +11,11 @@ const especies = [
 especieRouter.get('/', (req, res) => {
   res.status(200).json(especies)
 })
+
+especieRouter.get('/:id', (req, res) => {
+  const especie = especies.find((e) => e.id === req.params.id)
+  if (!especie) {
+    return res.status(404).json({ message: 'Especie no encontrada' })
+  }
+  res.status(200).json(especie)
+})
