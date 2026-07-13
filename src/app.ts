aecc0d37b80b1dc8c98a58d1f65eb93546b2551f
Marianela Altamirano <marianelaaltamirano.es@gmail.com>
2026-07-13 07:52:04 -0300

    Agrego manejo de rutas inexistentes (catch-all) con respuesta JSON consistente

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ app.use(express.json())
 
 app.use('/api/especies', especieRouter)
 
+app.use((_req, res) => {
+  res.status(404).json({ message: 'Recurso no encontrado' })
+})
+
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000')
 })
