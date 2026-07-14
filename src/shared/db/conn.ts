ca9c288148e0e56238152b8d407e8506b31d9e73
Marianela Altamirano <marianelaaltamirano.es@gmail.com>
2026-07-14 07:26:01 -0300

    Completo configuración del pool de conexiones MySQL

--- src/shared/db/conn.ts
+++ src/shared/db/conn.ts
@@ -7,4 +7,11 @@ export const pool = createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 })
