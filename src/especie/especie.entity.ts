7aa4ad62ac371950b60187fdfef136824dce85b0
Marianela Altamirano <marianelaaltamirano.es@gmail.com>
2026-07-15 08:10:06 -0300

    Convierto Especie en entidad MikroORM con decoradores (BaseEntity, @Entity, @Property)

--- src/especie/especie.entity.ts
+++ src/especie/especie.entity.ts
@@ -1,7 +1,11 @@
-export class Especie {
-  constructor(
-    public nombre: string,
-    public descripcion: string,
-    public id?: number
-  ) {}
+import { Entity, Property } from '@mikro-orm/decorators/legacy'
+import { BaseEntity } from '../shared/db/base.entity.js'
+
+@Entity()
+export class Especie extends BaseEntity {
+  @Property({ nullable: false, unique: true })
+  nombre!: string
+
+  @Property({ nullable: false })
+  descripcion!: string
 }
--- src/shared/db/orm.ts
+++ src/shared/db/orm.ts
@@ -1,10 +1,12 @@
 import { MikroORM } from '@mikro-orm/mysql'
 import { SqlHighlighter } from '@mikro-orm/sql-highlighter'
+import { ReflectMetadataProvider } from '@mikro-orm/decorators/legacy'
 import 'dotenv/config'
 
 export const orm = await MikroORM.init({
   entities: ['dist/**/*.entity.js'],
   entitiesTs: ['src/**/*.entity.ts'],
+  metadataProvider: ReflectMetadataProvider,
   dbName: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
