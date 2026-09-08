f19701d9d2e99b1c4aedbb25bc4de85286201d23
Valentina Sayago <valensayago05@gmail.com>
2026-09-08 18:47:32 -0300

    permito subida de imagenes sin autenticacion para el registro

--- src/upload/upload.routes.ts
+++ src/upload/upload.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 import { middlewareSubida, subirImagen } from './upload.controller.js'
-import { verificarToken } from '../auth/auth.middleware.js'
 
 export const uploadRouter = Router()
 
-uploadRouter.post('/', verificarToken, middlewareSubida, subirImagen)
+uploadRouter.post('/', middlewareSubida, subirImagen)
